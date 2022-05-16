d7c4ab8423ff20ccf004b62af05151c3a91f1f47
Evan Fleming <evan.gordon.fleming@gmail.com>
2022-05-15 18:05:57 -0700

    code splitting with react lazy/suspense (#16)
    * code splitting with react lazy/suspense
    
    * fixing test
    
    * removing home page from lazy loading

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,14 +1,17 @@
+import { ReactChild, Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router';
-import HomePage from '../HomePage';
-import ProfilePage from '../ProfilePage';
-import TopProfilesPage from '../TopProfilesPage';
-import ShopPage from '../ShopPage';
+import { Box, Center, Container, Spinner } from '@chakra-ui/react';
 import { Routes as RouteStrings } from '../../common/constants';
 import NavBar from './NavBar';
 import useToasts from './useToasts';
-import PageWrapper from '../../components/PageWrapper';
-import NotFound from '../../components/NotFound';
-import AboutPage from '../AboutPage';
+import ErrorBoundary from '../../components/ErrorBoundary';
+import HomePage from '../HomePage'; // We don't lazy load home page for SEO. We won't want a second network call involved in loading the landing page.
+
+const NotFound = lazy(() => import('../../components/NotFound'));
+const ProfilePage = lazy(() => import('../ProfilePage'));
+const TopProfilesPage = lazy(() => import('../TopProfilesPage'));
+const ShopPage = lazy(() => import('../ShopPage'));
+const AboutPage = lazy(() => import('../AboutPage'));
 
 function App() {
   useToasts();
@@ -17,7 +20,20 @@ function App() {
     <>
       <NavBar />
       <Routes>
-        <Route path={RouteStrings.PROFILE} element={<ProfilePage />} />
+        <Route
+          path={RouteStrings.PROFILE}
+          element={
+            <Suspense
+              fallback={
+                <Center>
+                  <Spinner />
+                </Center>
+              }
+            >
+              <ProfilePage />
+            </Suspense>
+          }
+        />
         <Route
           path={RouteStrings.TRENDING}
           element={
@@ -60,6 +76,27 @@ function App() {
         />
       </Routes>
     </>
+  );
+}
+
+function PageWrapper({ children }: { children: ReactChild }) {
+  return (
+    <Container maxW="container.lg">
+      <Box mt="15vh" />
+      <Center>
+        <ErrorBoundary>
+          <Suspense
+            fallback={
+              <Center>
+                <Spinner />
+              </Center>
+            }
+          >
+            {children}
+          </Suspense>
+        </ErrorBoundary>
+      </Center>
+    </Container>
   );
 }
 
