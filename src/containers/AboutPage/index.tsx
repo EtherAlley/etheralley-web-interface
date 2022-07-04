509b12e14d34b2988e139501b18fd545a6863125
Evan Fleming <evan.gordon.fleming@gmail.com>
2022-07-03 17:37:56 -0700

    Small updates to about page (#25)
    * small updates to about section
    
    * updating gifs

--- src/containers/AboutPage/index.tsx
+++ src/containers/AboutPage/index.tsx
@@ -24,19 +24,12 @@ function AboutPage() {
         <Text as="p" fontSize="lg" color="gray.300">
           {intl.formatMessage({
             id: 'what-is-etheralley-text-1',
-            defaultMessage: `As it stands today, there is an incredible amount of interesting interactions for users to have within the
-          Ethereum ecosystem. You can send and receive tokenized assets from your wallet, exchange them on a
-          decentralized exchange, use them to purchase the representation of physical assets, participate in governance
-          of projects you have interest in, stake your assets to earn rewards, and so much more.`,
-          })}
-        </Text>
-        <Text as="p" fontSize="lg" color="gray.300">
-          {intl.formatMessage({
-            id: 'what-is-etheralley-text-2',
-            defaultMessage: `Where there seems to be a gap, is in the ability to showcase these awesome interactions. That is, the ability
-          to show that you've interacted with and participated in many of these interesting technologies. EtherAlley is
-          an attempt to solve this problem, by giving users the ability to showcase their blockchain persona in a way
-          that's unique to them.`,
+            defaultMessage: `One of the biggest problems I personally find with the blockchain space today is the ability to actually show off all the cool interactions that I have on the blockchain. 
+            You can do things like send, receive, swap and lend tokenized assets. 
+            Participate in governance of your favorite organizations. 
+            Stake your assets to earn rewards, and so much more. 
+            But after having done all these things, you don't really have a space to show off that you've done them in an interesting way. 
+            EtherAlley is an attempt to improve on this problem by allowing you to build a completely customized profile showing off the favorite pieces of your blockchain persona.`,
           })}
         </Text>
         <Divider h={5} />
@@ -46,28 +39,13 @@ function AboutPage() {
         <Text as="p" fontSize="lg" color="gray.300">
           {intl.formatMessage({
             id: 'how-does-it-work-text',
-            defaultMessage: `EtherAlley gives you full control over what parts of your blockchain activity you want to display and in what
-          way you want to display them. Simply connect to your wallet in the top right corner of the screen and then
-          navigate to your profile to access the edit button. From there, the world is your oyster. Are you an NFT
-          collector and only want to display your most prized possessions? Are you a defi power user and want to show of
-          your best swaps? Do you have an eye for colors and want to come up with the perfect color palette to describe
-          you? Every customization is a click away`,
+            defaultMessage: `Simply connect to your wallet in the top right corner of the screen and then navigate to your profile to access the edit button. 
+            From there you are in complete control over everything to do with your profile. 
+            What colors are used. 
+            What NFT is your profile picture. 
+            What tokens, NFTs and stats you want to show on your profile, and so much more. `,
           })}
         </Text>
-        <Divider h={5} />
-        <Heading as="h2" mb={5}>
-          {intl.formatMessage({
-            id: 'customize-colors-header',
-            defaultMessage: `Completely customize your profile colors`,
-          })}
-        </Heading>
-        <Text as="p" fontSize="lg" color="gray.300">
-          {intl.formatMessage({
-            id: 'customize-colors-text',
-            defaultMessage: `Every color that appears on your profile can be customized to suit your style`,
-          })}
-        </Text>
-        <Image src="/gifs/customization_1.gif" />
         <Divider h={5} />
         <Heading as="h2" mb={5}>
           {intl.formatMessage({
@@ -82,7 +60,21 @@ function AboutPage() {
           on your profile can be added, removed, grouped, and ordered in whatever way suits you.`,
           })}
         </Text>
-        <Image src="/gifs/customization_2.gif" />
+        <Image src="/gifs/add-reorder-token.gif" />
+        <Divider h={5} />
+        <Heading as="h2" mb={5}>
+          {intl.formatMessage({
+            id: 'customize-colors-header',
+            defaultMessage: `Completely customize your profile colors`,
+          })}
+        </Heading>
+        <Text as="p" fontSize="lg" color="gray.300">
+          {intl.formatMessage({
+            id: 'customize-colors-text',
+            defaultMessage: `Every color that appears on your profile can be customized to suit your style`,
+          })}
+        </Text>
+        <Image src="/gifs/change-colors.gif" />
         <Divider h={5} />
         <Heading as="h2" mb={5}>
           {intl.formatMessage({
@@ -97,7 +89,7 @@ function AboutPage() {
           smart contract, being a beta tester and much more.`,
           })}
         </Text>
-        <Image src="/gifs/customization_3.gif" />
+        <Image src="/gifs/add-achievement.gif" />
       </VStack>
     </Box>
   );
